90a157ec6fcc898a7d4cee091237683d857bdf56
Electrolion <AMccollum71@gmail.com>
2021-11-10 09:49:32 -0500

    React implemented

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -30,6 +31,15 @@ const startServer = async () => {
 
 // Initialize the Apollo server
 startServer();
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
